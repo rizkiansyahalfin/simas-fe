605c177422c0d37ab8ba63f1e9b3d7afcfbfed6f
tsaqifmuwaffaq@gmail.com <tsaqifmuwaffaq@gmail.com>
2026-05-29 08:48:10 +0700

    feat: add admin inventory detail page with specs, condition, and loan history

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ import ZisManagement from "@/pages/admin/ZisManagement";
 import UserManagementPage from "./pages/admin/UserManagement";
 import DashboardPage from "@/pages/admin/DashboardPage";
 import AuditLogPage from "@/pages/admin/AuditLogPage";
+import InventoryDetailPage from "@/pages/admin/InventoryDetailPage";
 
 // Auth
 import Login from "@/pages/auth/Login";
@@ -294,6 +295,17 @@ function App() {
                         </ProtectedRoute>
                     }
                 />
+
+                <Route
+                 path='/admin/inventaris/:id'
+                 element={
+                     <ProtectedRoute resource='inventaris'>
+                         <AdminLayout>
+                             <InventoryDetailPage />
+                         </AdminLayout>
+                     </ProtectedRoute>
+                 }
+             />
 
                 {/* JAMAAH */}
                 <Route
